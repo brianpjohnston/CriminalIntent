686353cb8b27872857131f5db89ff63ca968794e
brianpjohnston <brian86j@yahoo.com>
2016-08-28 10:00:46 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by brian on 8/27/2016.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+
+
+    public Crime(){
+        //generate unique ID
+        mId = UUID.randomUUID()
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+}
