5565bae781b7728745c1e2889c60ac8b661b0091
Brian Johnston <brian86j@yahoo.com>
2016-08-29 20:50:42 -0400

    Chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -3,34 +3,28 @@ package com.bignerdranch.android.criminalintent;
 import java.util.Date;
 import java.util.UUID;
 
-/**
- * Created by brian on 8/27/2016.
- */
 public class Crime {
+
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
 
-
-
-
-    public Crime(){
-        //generate unique ID
+    public Crime() {
         mId = UUID.randomUUID();
         mDate = new Date();
     }
 
-    public UUID getmId() {
+    public UUID getId() {
         return mId;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
         return mTitle;
     }
 
-    public void setmTitle(String mTitle) {
-        this.mTitle = mTitle;
+    public void setTitle(String title) {
+        mTitle = title;
     }
 
     public Date getDate() {
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.criminalintent;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -14,59 +13,54 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+public class CrimeFragment extends Fragment {
 
-public class CrimeFragment extends android.support.v4.app.Fragment {
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
-    private CheckBox mSolvedCheckBox;
-
+    private CheckBox mSolvedCheckbox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ;
         mCrime = new Crime();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View V = inflater.inflate(R.layout.fragment_crime, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        mTitleField = (EditText) V.findViewById((R.id.crime_title));
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //This space left blank
+
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int count, int after) {
-                mCrime.setmTitle(s.toString());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                //left blank
 
             }
         });
 
-        mDateButton = (Button) V.findViewById(R.id.crime_date);
-        mDateButton.setText((DateFormat.format("EEEE, MMM d, yyyy", mCrime.getDate())));
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(DateFormat.format("EEEE, MMM d, yyyy", mCrime.getDate()));
         mDateButton.setEnabled(false);
 
-        mSolvedCheckBox = (CheckBox) V.findViewById(R.id.crime_solved);
-        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
             }
-
         });
 
-        return V;
+        return v;
     }
-
-
 }
